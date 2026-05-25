d9caf7ceb641756c7d08537ddf1c6fac848264dd
Chad Francis <yamanote1138@gmail.com>
2026-05-25 00:39:00 -0600

    Add Vitest test infrastructure

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config'
+import { fileURLToPath, URL } from 'node:url'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'jmri-client': fileURLToPath(new URL('./node_modules/jmri-client/dist/browser/jmri-client.js', import.meta.url))
+    }
+  },
+  test: {
+    environment: 'jsdom',
+  },
+})
